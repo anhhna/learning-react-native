bfb17518e4abd33172bab7b06a1f73edf7e69398
AG\AG <anhhna@gmail.com>
2017-09-19 06:47:50 +0700

    add Examples, Style Guides, Exercises, Homeworks

--- L2_ES6/index.android.js
+++ L2_ES6/index.android.js
@@ -1,9 +1,25 @@
-import e1 from './1_primitive_types.js';
+// Examples
+//import e1 from './1_primitive_types.js';
 //import e2 from './2_complex_types.js';
-//import ex from './ex.js';
+//import e3 from './3_reference.js';
+//import e4 from './4_object.js';
+//import e5 from './5_array.js'
+//import e6 from './6_function.js';
+//import e7 from './7_text.js';
+//import e8 from './8_class.js';
+//import e9 from './9_module.js';
+//import e10 from './10_math.js';
+//import e11 from './11_set.js';
+
+// Style Guides
 //import sg6 from './sg6_side_effects.js';
-// import hw1 from './hw1.js';
-// import hw2 from './hw2.js';
-// import hw3 from './hw3.js';
-// import hw4 from './hw4.js';
-// import hw5 from './hw5.js';
+
+// Exercises
+//import ex from './ex.js';
+
+// Homeworks
+//import hw1 from './hw1.js';
+//import hw2 from './hw2.js';
+//import hw3 from './hw3.js';
+//import hw4 from './hw4.js';
+//import hw5 from './hw5.js';
